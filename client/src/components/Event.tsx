415ed121f1e6c2b8a2fb1b8d45d0ebc6ec5cc477
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-16 13:47:42 +0100

    Fix: Merge branches

--- client/src/components/Event.tsx
+++ client/src/components/Event.tsx
@@ -16,7 +16,6 @@ type EventProps = {
 const Event = ({ event, onDelete, formPath }: EventProps) => {
   return (
     <div className='event-div'>
-      {/* <div id='event-list'> */}
       <div className='left'>
         <div className='event-day-month'>
           {moment(event.date).format('Do[\n]MMM')}
@@ -26,25 +25,8 @@ const Event = ({ event, onDelete, formPath }: EventProps) => {
           <div className='list-date'>
             <p>{moment(event.date).format('hh:mm a - MMMM  Do, YYYY')}</p>
           </div>
-<<<<<<< HEAD
-          <div className='event-outer'>
-            <div className='list-title'>{event.name} </div>
-            <div className='list-date'>
-              <p>{moment(event.date).format('hh:mm a - MMMM  Do, YYYY')}</p>
-            </div>
-            <div className='list-venue'>
-              <p>{event.venue}</p>
-            </div>
-          </div>
-          <div className='btn-dev'>
-            <Link href={`${formPath}${event._id}`}>
-              <button className='btn'>Click</button>
-            </Link>
-            <FaTrash className='dele-btn' onClick={() => onDelete(event._id)} />
-=======
           <div className='list-venue'>
             <p>{event.venue}</p>
->>>>>>> client/cypress
           </div>
         </div>
         <div className='btn-dev'>
@@ -54,7 +36,6 @@ const Event = ({ event, onDelete, formPath }: EventProps) => {
           <FaTrash className='dele-btn' onClick={() => onDelete(event._id)} />
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 };
